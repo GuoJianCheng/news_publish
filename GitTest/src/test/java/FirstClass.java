7c78ebfb6f0c01b296b70d53daab15f85d4b956d
guojiancheng <2557471827@qq.com>
2019-12-10 15:51:31 +0800

    第一次修改

--- GitTest/src/test/java/FirstClass.java
+++ GitTest/src/test/java/FirstClass.java
@@ -2,4 +2,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello Git");
     }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
